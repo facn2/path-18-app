038cdf3a1a18c3dab14f545f6e3cb11844cd7795
matthewdking <matthew.d.king@outlook.com>
2017-11-20 21:26:47 +0200

    adds travis test env #32

--- database/db_connection.js
+++ database/db_connection.js
@@ -2,8 +2,9 @@ const { Pool } = require('pg');
 require('env2')('./config.env');
 
 const dbUrl =
-  (process.env.ENV !== 'testing' && process.env.DATABASE_URL) ||
-  process.env.TESTING_DATABASE_URL;
+  process.env.ENV !== 'testing'
+    ? process.env.DATABASE_URL
+    : process.env.TESTING_DATABASE_URL;
 
 const pool = new Pool({
   connectionString: dbUrl,
