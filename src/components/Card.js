9c66a9b91710ce6df85a0f84718a42c34f0125cb
matthewdking <matthew.d.king@outlook.com>
2017-11-26 10:31:23 +0200

    adds style to Card #43 :lipstick:

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,29 @@ const CardContainer = styled.div`
   background-color: palevioletred;
   height: 20rem;
   margin: 2rem;
+  overflow: auto;
+`;
+
+const CardImageWrapper = styled.div`
+  height: 15rem;
+  background-color: blue;
+  margin: 1rem;
+`;
+
+const CareerTitleWrapper = styled.div`
+  color: black;
+  bottom: 0.75rem;
+  margin-left: 1rem;
+  font-size: 1.25rem;
 `;
 
 const Card = () => {
-  return <CardContainer />;
+  return (
+    <CardContainer>
+      <CardImageWrapper />
+      <CareerTitleWrapper>This is the career title</CareerTitleWrapper>
+    </CardContainer>
+  );
 };
 
 export default Card;
--- src/containers/Careers.js
+++ src/containers/Careers.js
@@ -5,13 +5,11 @@ import Card from '../components/Card';
 const CareersContainer = styled.div`
   color: blue;
 `;
-const MainTitle = styled.h1``;
 
 class Careers extends Component {
   render() {
     return (
       <CareersContainer>
-        <MainTitle> This is Career! </MainTitle>
         <Card />
       </CareersContainer>
     );
