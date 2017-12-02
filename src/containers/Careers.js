34cf7f79a15a4df3beab1b941043d894e87f0269
matthewdking <matthew.d.king@outlook.com>
2017-12-02 15:47:17 +0200

    adds swiping using swing and redux swing #28 :sparkles:

--- src/containers/Careers.js
+++ src/containers/Careers.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Card from '../components/Card';
+import Swing from 'react-swing';
+import { Direction } from 'swing';
+import CareerCard from '../components/Card';
 import LikeAndDislikeButtons from '../components/LikeAndDislikeButtons';
 
 const CareersContainer = styled.div`
@@ -12,11 +14,43 @@ const CareersContainer = styled.div`
   flex-direction: column;
 `;
 
+const SwingWrapper = styled.div`
+  position: relative;
+  height: 70%;
+  margin: 0 2rem;
+`;
+
 class Careers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stack: null,
+    };
+  }
+
   render() {
     return (
       <CareersContainer>
-        <Card />
+        <SwingWrapper id="viewport">
+          <Swing
+            tagName="div"
+            setStack={stack => this.setState({ stack: stack })}
+            ref="stack"
+            throwout={e => console.log(e)}
+            config={{
+              allowedDirections: [Direction.LEFT, Direction.RIGHT],
+              maxRotation: 5,
+            }}
+          >
+            <div className="cardwrapper" ref="card1">
+              <CareerCard />
+            </div>
+            <div className="cardwrapper" ref="card2">
+              <CareerCard />
+            </div>
+          </Swing>
+        </SwingWrapper>
         <LikeAndDislikeButtons />
       </CareersContainer>
     );
