871fbf4fc6c25428347f31ba4cee83d6db13672a
matthewdking <matthew.d.king@outlook.com>
2017-12-03 21:36:26 +0200

    styles matched careers list #7 :lipstick:

--- src/containers/LikedCareers.js
+++ src/containers/LikedCareers.js
@@ -4,21 +4,66 @@ import styled from 'styled-components';
 import fetchLikedCareers from '../actions/fetch_liked_careers';
 
 const LikedContainer = styled.div`
-  color: green;
+  width: 100%;
 `;
-const MainTitle = styled.h1``;
+
+const LikedList = styled.ul`
+  width: 100%;
+  height: 80%;
+`;
+
+const LikedListItem = styled.li`
+  width: 100%;
+  height: 3rem;
+  border-bottom: 0.1rem solid gainsboro;
+  border-width: thin;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 1.2rem;
+  color: #455a64;
+`;
+
+const LikedListText = styled.p`
+  text-align: right;
+  padding-right: 5%;
+`;
+
+const LikedListIcon = styled.i`
+  padding-right: 0.9rem;
+  font-size: 1.5rem;
+  color: #d32f2f;
+  cursor: pointer;
+  user-select: none;
+`;
 
 class LikedCareers extends Component {
   componentDidMount() {
     this.props.fetchLikedCareers();
   }
 
+  likedCareersList = () => {
+    if (
+      this.props.matchedCareers.dataFetched &&
+      this.props.matchedCareers.likedCareers.length
+    ) {
+      return (
+        <LikedList>
+          {this.props.matchedCareers.likedCareers.map(career => {
+            return (
+              <LikedListItem key={`career${career.id}`}>
+                <LikedListText>{career.title_ar}</LikedListText>
+                <LikedListIcon className="material-icons">clear</LikedListIcon>
+              </LikedListItem>
+            );
+          })}
+        </LikedList>
+      );
+    }
+  };
+
   render() {
-    return (
-      <LikedContainer>
-        <MainTitle>This is LikedCareers</MainTitle>
-      </LikedContainer>
-    );
+    return <LikedContainer>{this.likedCareersList()}</LikedContainer>;
   }
 }
 
