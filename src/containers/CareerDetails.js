47047c83d4b18725189229b8f0c94480282c5882
matthewdking <matthew.d.king@outlook.com>
2017-12-11 14:22:34 +0200

    renders specific career data #8 :speech_balloon:

--- src/containers/CareerDetails.js
+++ src/containers/CareerDetails.js
@@ -67,71 +67,81 @@ const TitleTagWrapper = styled.div`
 
 class CareerDetails extends Component {
   componentDidMount() {
-    console.log(this.props.match.params.id);
     this.props.fetchCareerById({ career_id: this.props.match.params.id });
   }
 
+  showDetails = () => {
+    if (this.props.careerDetail.dataFetched) {
+      return (
+        <div>
+          <TopSection>
+            <TitleTagWrapper>
+              <CareerTitle>
+                {this.props.careerDetail.career[0].title_ar}
+              </CareerTitle>
+              <CareerTagline>
+                {this.props.careerDetail.career[0].tagline_ar}
+              </CareerTagline>
+            </TitleTagWrapper>
+            <CareerLogo className="material-icons">
+              {this.props.careerDetail.career[0].icon_name}
+            </CareerLogo>
+          </TopSection>
+
+          <DetailSection>
+            <Description>
+              {this.props.careerDetail.career[0].description_ar}
+            </Description>
+            <TitleWrapper>
+              <SectionTitle>Salary</SectionTitle>
+            </TitleWrapper>
+            <TableContainer>
+              <TableColumn>
+                <Start>
+                  ₪ {this.props.careerDetail.career[0].salary_start}
+                </Start>
+                <TenYear>
+                  ₪ {this.props.careerDetail.career[0].salary_ten_year}
+                </TenYear>
+              </TableColumn>
+              <TableColumn>
+                <SalaryStart>Junior </SalaryStart>
+                <SalaryTenYear>Senior</SalaryTenYear>
+              </TableColumn>
+            </TableContainer>
+          </DetailSection>
+
+          <DetailSection>
+            <TitleWrapper>
+              <Icon className="material-icons">keyboard_arrow_left</Icon>
+              <SectionTitle>Haifa University</SectionTitle>
+              <Icon className="material-icons">keyboard_arrow_right</Icon>
+            </TitleWrapper>
+            <TableContainer>
+              <TableColumn>
+                <ReqBagrut> 100.00 </ReqBagrut>
+                <ReqTawjihi> 99.00 </ReqTawjihi>
+                <ReqPsyc> 740 </ReqPsyc>
+              </TableColumn>
+              <TableColumn>
+                <UserBagrut> 101.5 </UserBagrut>
+                <UserTawjihi> - </UserTawjihi>
+                <UserPsyc> 720 </UserPsyc>
+              </TableColumn>
+              <TableColumn>
+                <GradeBagrut>Bagrut</GradeBagrut>
+                <GradeTawjihi>Tawjihi</GradeTawjihi>
+                <GradePsyc>Psychometric</GradePsyc>
+              </TableColumn>
+            </TableContainer>
+          </DetailSection>
+        </div>
+      );
+    }
+  };
+
   render() {
-    console.log(this.props);
-    return (
-      <CareerContainer>
-        <TopSection>
-          <TitleTagWrapper>
-            <CareerTitle>Doctor</CareerTitle>
-            <CareerTagline>Lorem ipsum dolor sit amet,</CareerTagline>
-          </TitleTagWrapper>
-          <CareerLogo className="material-icons">local_hospital</CareerLogo>
-        </TopSection>
-
-        <DetailSection>
-          <Description>
-            A physician, medical practitioner, medical doctor, or simply doctor
-            is a professional who practises medicine, which is concerned with
-            promoting, maintaining, or restoring health through the study,
-            diagnosis, and treatment of disease, injury, and other physical and
-            mental impairments.
-          </Description>
-          <TitleWrapper>
-            <SectionTitle>Salary</SectionTitle>
-          </TitleWrapper>
-          <TableContainer>
-            <TableColumn>
-              <Start> ₪ 7,000 </Start>
-              <TenYear> ₪ 25,000 </TenYear>
-            </TableColumn>
-            <TableColumn>
-              <SalaryStart>Junior </SalaryStart>
-              <SalaryTenYear>Senior</SalaryTenYear>
-            </TableColumn>
-          </TableContainer>
-        </DetailSection>
-
-        <DetailSection>
-          <TitleWrapper>
-            <Icon className="material-icons">keyboard_arrow_left</Icon>
-            <SectionTitle>Haifa University</SectionTitle>
-            <Icon className="material-icons">keyboard_arrow_right</Icon>
-          </TitleWrapper>
-          <TableContainer>
-            <TableColumn>
-              <ReqBagrut> 100.00 </ReqBagrut>
-              <ReqTawjihi> 99.00 </ReqTawjihi>
-              <ReqPsyc> 740 </ReqPsyc>
-            </TableColumn>
-            <TableColumn>
-              <UserBagrut> 101.5 </UserBagrut>
-              <UserTawjihi> - </UserTawjihi>
-              <UserPsyc> 720 </UserPsyc>
-            </TableColumn>
-            <TableColumn>
-              <GradeBagrut>Bagrut</GradeBagrut>
-              <GradeTawjihi>Tawjihi</GradeTawjihi>
-              <GradePsyc>Psychometric</GradePsyc>
-            </TableColumn>
-          </TableContainer>
-        </DetailSection>
-      </CareerContainer>
-    );
+    return <CareerContainer>{this.showDetails()}</CareerContainer>;
   }
 }
 
@@ -141,6 +151,7 @@ const mapDispatchToProps = dispatch => ({
 
 const mapStateToProps = state => ({
   matchedCareers: state.matchedCareers,
+  careerDetail: state.careerDetail,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CareerDetails);
