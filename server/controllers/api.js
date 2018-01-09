e4370f5ba7867a4b699abf6135a9d20b02e69fe8
matthewdking <matthew.d.king@outlook.com>
2018-01-09 09:53:11 +0200

    adds userIds to all db queries from request #3 :ambulance:

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,26 +1,25 @@
-const getAllCareersDb = require('../../database/queries/get_all_careers');
-const getLikedCareersDb = require('../../database/queries/get_liked_careers');
-const likeCareerDb = require('../../database/queries/like_career');
-const unlikeCareerDb = require('../../database/queries/unlike_career');
-const getCareerById = require('../../database/queries/career_by_id');
-const getUniByCareerId = require('../../database/queries/get_career_uni');
-const getUserGrades = require('../../database/queries/get_user_grades');
+const getAllCareersDb = require("../../database/queries/get_all_careers");
+const getLikedCareersDb = require("../../database/queries/get_liked_careers");
+const likeCareerDb = require("../../database/queries/like_career");
+const unlikeCareerDb = require("../../database/queries/unlike_career");
+const getCareerById = require("../../database/queries/career_by_id");
+const getUniByCareerId = require("../../database/queries/get_career_uni");
+const getUserGrades = require("../../database/queries/get_user_grades");
 
 const allCareers = async (request, response) => {
-  console.log(request);
-  const userId = 1;
+  const userId = request.user.id;
   try {
     const careers = await getAllCareersDb(userId);
     response.send(careers);
   } catch (error) {
     console.log(error);
-    response.send({ error: 'Error getting all careers' });
+    response.send({ error: "Error getting all careers" });
   }
 };
 
 const careerDetails = async (request, response) => {
   const careerId = request.params.id;
-  const userId = 1;
+  const userId = request.user.id;
   const details = {};
   try {
     details.career = await getCareerById(careerId);
@@ -29,42 +28,42 @@ const careerDetails = async (request, response) => {
     response.send(details);
   } catch (error) {
     console.log(error);
-    response.send({ error: 'Error getting career details' });
+    response.send({ error: "Error getting career details" });
   }
 };
 
 const likedCareers = async (request, response) => {
-  const userId = 1;
+  const userId = request.user.id;
   try {
     const careers = await getLikedCareersDb(userId);
     response.send(careers);
   } catch (error) {
     console.log(error);
-    response.send({ error: 'Error getting liked careers' });
+    response.send({ error: "Error getting liked careers" });
   }
 };
 
 const likeCareer = async (request, response) => {
-  const userId = 1;
+  const userId = request.user.id;
   const careerId = request.body.career_id;
   try {
     const like = await likeCareerDb(userId, careerId);
     response.send(like);
   } catch (error) {
     console.log(error);
-    response.send({ error: 'Error liking career' });
+    response.send({ error: "Error liking career" });
   }
 };
 
 const unlikeCareer = async (request, response) => {
-  const userId = 1;
+  const userId = request.user.id;
   const careerId = request.params.id;
   try {
     const unlike = await unlikeCareerDb(userId, careerId);
     response.send(unlike);
   } catch (error) {
     console.log(error);
-    response.send({ error: 'Error unliking career' });
+    response.send({ error: "Error unliking career" });
   }
 };
 
