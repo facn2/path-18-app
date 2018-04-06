aa0e9a00a75798c3010c2a7aa60a4f9342fca7d1
Mario <mario.saliba98@gmail.com>
2018-04-06 18:22:28 +0300

    creates the update query for the user's grades rel #16

---
+++ database/queries/update_user.js
@@ -0,0 +1,28 @@
+const dbConnection = require("../db_connection");
+
+const updateUserGrades = (id, grades) => {
+  const updateUserGradesQuery = `
+  UPDATE users
+    SET
+      grade_bagrut = $2,
+      grade_psychometric = $3,
+      grade_tawjihi = $4
+    WHERE
+      id = $1
+`;
+  return new Promise((resolve, reject) => {
+    dbConnection.query(
+      updateUserGradesQuery,
+      [id, ...grades],
+      (error, response) => {
+        if (error) {
+          reject(error);
+        }
+        console.log(response);
+        resolve(response);
+      }
+    );
+  });
+};
+
+module.exports = updateUserGrades;
