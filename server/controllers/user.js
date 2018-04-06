03c8099d8cde1607eaf099ed9cf40d336025d677
Mario <mario.saliba98@gmail.com>
2018-04-06 18:21:07 +0300

    add the update user controller rel #16

---
+++ server/controllers/user.js
@@ -0,0 +1,18 @@
+const updateUserGrades = require("../../database/queries/update_user");
+
+module.exports.update = async (req, res) => {
+  console.log(req.body);
+  const { id, bagrut_grade, psychometric_grade, tawjihi_grade } = req.body;
+
+  try {
+    await updateUserGrades(id, [
+      bagrut_grade || 0,
+      psychometric_grade || 0,
+      tawjihi_grade || 0
+    ]);
+
+    res.send("Success");
+  } catch (error) {
+    res.send({ error: error.message });
+  }
+};
