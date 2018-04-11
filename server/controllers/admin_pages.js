57962506d6ed10dea8d9ae63f58953625a225f4b
Mario <mario.saliba98@gmail.com>
2018-04-11 19:44:15 +0300

    create the single career controler

--- server/controllers/admin_pages.js
+++ server/controllers/admin_pages.js
@@ -1,9 +1,21 @@
-const { getAllCareers } = require('../../database/queries/get_all_careers');
+const {
+  getAllCareers,
+  getCareer,
+} = require('../../database/queries/get_all_careers');
 
 const allCareersPage = async (req, res) => {
   try {
     const careers = await getAllCareers();
     res.render('allCareers', { careers });
+  } catch (e) {
+    res.send(e);
+  }
+};
+
+const singleCareerPage = async (req, res) => {
+  try {
+    const career = await getCareer(req.params.id);
+    res.render('singleCareer', { career });
   } catch (e) {
     res.send(e);
   }
