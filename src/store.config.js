89336cfa30d62bc6d4676da11cc7990589cef3e7
Mario <mario.saliba98@gmail.com>
2018-04-20 18:13:57 +0300

    comment redux logger for now

--- src/store.config.js
+++ src/store.config.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import reducers from './reducers/index';
 
 const store = createStore(
   reducers,
   {},
-  applyMiddleware(thunk, promiseMiddleware(), logger)
+  applyMiddleware(thunk, promiseMiddleware()),
 );
 
 export default store;
